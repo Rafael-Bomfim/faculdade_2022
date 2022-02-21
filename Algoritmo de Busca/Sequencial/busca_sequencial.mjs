c828a0983042924dd1565f7f68073742c4d6e279
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-02-21 20:29:14 -0300

    Busca sequencial

---
+++ Algoritmo de Busca/Sequencial/busca_sequencial.mjs
@@ -0,0 +1,19 @@
+function buscaSequencial(vetor, valorBusca) {
+    for(let i = 0; i < vetor.length; i++) {
+        if(vetor[i] === valorBusca) 
+        return i //encontrou valor
+    }
+    return -1 //se o valor busca não existir
+}
+// const frutas = ['laranja', 'maça', 'uva', 'pera', 'jaboticaba', 'limao', 'mamão']
+
+// console.log('Posição de uva: ', buscaSequencial(frutas, 'uva'))
+// console.log('Posição de uva: ', buscaSequencial(frutas, 'abacate'))
+// console.log('Posição de uva: ', buscaSequencial(frutas, 'limão'))
+// console.log('Posição de uva: ', buscaSequencial(frutas, 'limao'))
+
+import { nomes } from './vetor-nomes.mjs'
+
+console.time('COMEÇO....')
+console.log('Posição de GUSTAVO: ', buscaSequencial(nomes,'GUSTAVO'))
+console.timeEnd('FIM')

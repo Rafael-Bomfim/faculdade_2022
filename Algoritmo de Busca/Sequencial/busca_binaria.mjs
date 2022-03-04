43cdaf83f2138607bb4c84809984129f18207ed2
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-03-04 20:38:50 -0300

    Busca binária

---
+++ Algoritmo de Busca/Sequencial/busca_binaria.mjs
@@ -0,0 +1,26 @@
+let num = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99]
+
+function buscaBinaria(vetor, valorBusca) {
+    let ini = 0
+    let fim = vetor.length -1
+
+    while(fim >= ini) {
+        let meio = Math.floor((ini + fim) / 2) //resultado da divisão sempre será um número inteiro
+
+        if(valorBusca === vetor[meio]) {
+            return meio
+        }
+        else if(valorBusca > vetor[meio]) {
+            ini = meio +1
+        }
+        else {
+            fim = meio -1
+        }
+    }
+    return -1
+}
+console.log('Posição de 22:', buscaBinaria(num, 88))
+
+import {nomes} from './listas/vetor-nomes.mjs'
+
+console.log('Posição de ALEXANDRE: ', buscaBinaria(nomes, 'ALEXANDRE'))

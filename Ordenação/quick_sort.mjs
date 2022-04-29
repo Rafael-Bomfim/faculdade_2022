1dd72c1cde372ab4fe0fc946d22a7b3b73b9d82d
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-04-29 20:47:37 -0300

    Ordenação com Quick Sort

---
+++ Ordenação/quick_sort.mjs
@@ -0,0 +1,23 @@
+function quickSort(vetor, ini = 0, fim = vetor.length - 1) { //inicio e fim do vetor
+    if (fim <= ini) { //se o vetor tiver somente um elemento
+        return vetor
+    }
+    if (ini < fim) {
+        let pivo = vetor[fim];
+        let i = ini - 1;
+        for (let j = ini; j < fim; j++) {
+            if (vetor[j] <= pivo) {
+                i++;
+                [vetor[i], vetor[j]] = [vetor[j], vetor[i]];
+            }
+        }
+        [vetor[i + 1], vetor[fim]] = [vetor[fim], vetor[i + 1]];
+        quickSort(vetor, ini, i);
+        quickSort(vetor, i + 2, fim);
+    }
+    return vetor;
+}
+
+let nums = [2,5,7,1,6,3,4]
+quickSort(nums)
+console.log(nums)
